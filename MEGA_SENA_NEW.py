814e1a71bca4633dc6dc133dfad8211c7d25576e
Gustavo Duran <81047389+GustavoDuranBR@users.noreply.github.com>
2021-04-23 17:31:29 -0300

    Add arquivo .py
    Add arquivo .py com algumas correções

--- MEGA_SENA_NEW.py
+++ MEGA_SENA_NEW.py
@@ -1,13 +1,13 @@
 from random import *
 
-print('=' * 27)
+print('$' * 27)
 print('     Palpite MEGA-SENA     ')
-print('=' * 27)
+print('$' * 27)
 
 
 def contador(x):
-    print('=' * 30)
     print(f'Os números da sorte são: \n{x}')
+    print('$' * 30)
 
 
 lista = []
@@ -25,3 +25,5 @@ contador(sorted(lista2))
 for i in sample(range(1, 61), 6):
     lista3.append(i)
 contador(sorted(lista3))
+
+print('\n$$$ Boa sorte $$$')
