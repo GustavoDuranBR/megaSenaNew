c73e2c8e383b9b74158b9bb96d79fed936d59249
GustavoDuranBR <gustavoduran22@gmail.com>
2021-04-23 16:44:45 -0300

    Primeiro commit Mega_Sena_New

---
+++ MEGA_SENA_NEW.py
@@ -0,0 +1,27 @@
+from random import *
+
+print('=' * 27)
+print('     Palpite MEGA-SENA     ')
+print('=' * 27)
+
+
+def contador(x):
+    print('=' * 30)
+    print(f'Os números da sorte são: \n{x}')
+
+
+lista = []
+lista2 = []
+lista3 = []
+
+for i in sample(range(1, 61), 6):
+    lista.append(i)
+contador(sorted(lista))
+
+for i in sample(range(1, 61), 6):
+    lista2.append(i)
+contador(sorted(lista2))
+
+for i in sample(range(1, 61), 6):
+    lista3.append(i)
+contador(sorted(lista3))
